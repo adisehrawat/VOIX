fc2a57de5f6d610d231e1c5d1da29accb5b90d16
adisehrawat <sehrawataditya22@gmail.com>
2025-10-30 21:34:42 +0530

    fixed build 4th  time

--- frontend/voix-app/babel.config.js
+++ frontend/voix-app/babel.config.js
@@ -1,21 +1,16 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: [
-        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      ],
-      plugins: [
-        "nativewind/babel",
-        [
-          "react-native-css-interop/plugin",
-          {
-            searchPaths: ["./app", "./src"],
-            // Add this for production builds
-            mode: "transformOnly"
-          }
-        ],
-        // Reanimated must be last
-        "react-native-reanimated/plugin"
-      ],
-    };
+  api.cache(true);
+  return {
+    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
+    plugins: [
+    //   [
+    //     "nativewind/babel",
+    //     {
+    //       mode: "transformOnly",
+    //     },
+    //   ],
+      // Reanimated must be last
+      "react-native-reanimated/plugin",
+    ],
   };
+};
