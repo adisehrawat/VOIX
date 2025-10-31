56d9acbbbe203750b11c331c8b69cd125107a6ba
adisehrawat <sehrawataditya22@gmail.com>
2025-10-31 22:05:21 +0530

    updated code

--- frontend/voix-app/app.config.js
+++ frontend/voix-app/app.config.js
@@ -1,66 +1,60 @@
 export default {
-  "expo": {
-    "name": "Voix",
-    "slug": "voix-app",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/images/voixlogo.png",
-    "scheme": "voixapp",
-    "userInterfaceStyle": "automatic",
-    "newArchEnabled": true,
-    "ios": {
-      "supportsTablet": true,
-      "infoPlist": {
-        "NSAppTransportSecurity": {
-          "NSAllowsArbitraryLoads": true,
-          "NSExceptionDomains": {
-            "localhost": {
-              "NSExceptionAllowsInsecureHTTPLoads": true
+  expo: {
+    name: "Voix",
+    slug: "voix-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/voixlogo.png",
+    scheme: "voixapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      infoPlist: {
+        NSAppTransportSecurity: {
+          NSAllowsArbitraryLoads: true,
+          NSExceptionDomains: {
+            localhost: {
+              NSExceptionAllowsInsecureHTTPLoads: true,
             },
             "127.0.0.1": {
-              "NSExceptionAllowsInsecureHTTPLoads": true
-            }
-          }
-        }
-      }
-    },
-    "build": {
-      "production": {
-        "android": {
-          "buildType": "apk",
-          "cache": {
-            "key": "nativewind-cache"
-          }
-        }
-      }
-    },
-    "android": {
-      "adaptiveIcon": {
-        "backgroundColor": "#E6F4FE",
-        "foregroundImage": "./assets/images/voixlogo.png"
+              NSExceptionAllowsInsecureHTTPLoads: true,
+            },
+          },
+        },
       },
-      "edgeToEdgeEnabled": true,
-      "predictiveBackGestureEnabled": false,
-      "package": "com.adityavoix.voixapp"
     },
-    "web": {
-      "output": "static",
-      "favicon": "./assets/images/voixlogo.png",
-      "bundler": "metro"
+    build: {
+      production: {
+        android: {
+          buildType: "apk",
+          cache: {
+            key: "nativewind-cache",
+          },
+        },
+      },
     },
-    "plugins": [
-      "expo-router",
-      "expo-font"
-    ],
-    "experiments": {
-      "typedRoutes": true
+    android: {
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      usesCleartextTraffic: true,
+      package: "com.voixaditya.voixapp",
     },
-    "extra": {
-      "router": {},
-      "eas": {
-        "projectId": "49029896-19d5-49de-b96a-1d26c3b2b5ee"
-      }
+    web: {
+      output: "static",
+      favicon: "./assets/images/voixlogo.png",
+      bundler: "metro",
     },
-    "owner": "adityavoix"
-  }
+    plugins: ["expo-router", "expo-font"],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "5bf4cc8d-6868-410c-a213-2f0787baf930",
+      },
+    },
+    owner: "voixaditya",
+  },
 };
